a56a995b101534e7fa9dc63da4c6e4b666ac92ba
vasanth <swaminathanvasanth.r@gmail.com>
2018-08-01 14:25:58 +0530

    Migrating ManagementAPIs to use Undertow

---
+++ src/rbccps/smartcity/IDEAM/APIs/cdxapis.java
@@ -0,0 +1,28 @@
+package rbccps.smartcity.IDEAM.APIs;
+
+import io.undertow.Handlers;
+import io.undertow.Undertow;
+import io.undertow.server.handlers.PathHandler;
+import io.undertow.servlet.Servlets;
+import io.undertow.servlet.api.DeploymentInfo;
+import io.undertow.servlet.api.DeploymentManager;
+
+public class cdxapis {
+
+	static DeploymentInfo servletBuilder;
+	static DeploymentManager manager;
+	
+	public static void main(String... args) throws Exception {
+		servletBuilder = Servlets.deployment().setClassLoader(cdxapis.class.getClassLoader());
+		servletBuilder.setDeploymentName("cdx").setContextPath("/cdx");
+		
+		servletBuilder.addServlets(Servlets.servlet("redirect", new RequestRedirect().getClass()).addMapping("/redirect"));
+		
+		manager = Servlets.defaultContainer().addDeployment(servletBuilder);
+		manager.deploy();
+		PathHandler path = Handlers.path(Handlers.redirect("/cdx")).addPrefixPath("/cdx", manager.start());
+		Undertow server = Undertow.builder().addHttpListener(8888, "localhost").setHandler(path).build();
+		server.start();
+	}
+
+}
