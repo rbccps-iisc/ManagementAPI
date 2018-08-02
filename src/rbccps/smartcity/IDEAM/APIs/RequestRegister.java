bfc9fb047cbf9090e338a4c3b625153ae437fd30
vasanth <swaminathanvasanth.r@gmail.com>
2018-08-02 17:39:02 +0530

    Modifying register API to use Undertow Servlet

--- src/rbccps/smartcity/IDEAM/APIs/RequestRegister.java
+++ src/rbccps/smartcity/IDEAM/APIs/RequestRegister.java
@@ -4,16 +4,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.POST;	
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-
+import javax.servlet.http.HttpServletResponse;
 import org.json.simple.JSONObject;
 
 import rbccps.smartcity.IDEAM.registerapi.kong.Register;
@@ -21,13 +17,12 @@ import rbccps.smartcity.IDEAM.registerapi.parser.createEntityJSONParser;
 import rbccps.smartcity.IDEAM.registerapi.parser.entity;
 import rbccps.smartcity.IDEAM.registerapi.deregister.*;
 
-@Path("/register")
-public class RequestRegister {
+public class RequestRegister extends HttpServlet {
 
 	private static final createEntityJSONParser DeleteEntityJSONParser = null;
 
 	Register register = new Register();
-	
+
 	static JSONObject responseObj;
 	static String response;
 	static String authorization;
@@ -37,15 +32,33 @@ public class RequestRegister {
 	static String X_Consumer_Groups;
 	static String body;
 	static String security_level;
+	static String returnData;
+	static final int SERVICE_UNAVAILABLE = 503;
+	static final int CONFLICT = 409;
+	static final int BAD_REQUEST = 400;
+	static PrintWriter out;
 	
-	@POST
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response getAPIKey(@Context HttpServletRequest request) {
-		
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		response.sendRedirect("http://rbccps.org/smartcity/");
+		return;
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		resp = (HttpServletResponse) getAPIKey(req, resp);
+		out = resp.getWriter();
+		resp.setContentType("application/json");
+		out.print(returnData);
+	}
+
+	public HttpServletResponse getAPIKey(HttpServletRequest request, HttpServletResponse resp) {
+
 		System.out.println("------------");
 		System.out.println(request.getRequestURI());
 		System.out.println("------------");
-		
+
 		try {
 			getHeaderInfo(request);
 			body = getBody(request);
@@ -54,58 +67,52 @@ public class RequestRegister {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	
-		String returnData = createEntityJSONParser.JSONParser();
-		System.out.println(returnData);	
-		
-		if(returnData.contains("ID already used"))
-			 return Response.status(Response.Status.CONFLICT).entity(returnData).build();
-		
-		else if(returnData.contains("uCat update Failure")||returnData.contains("Server Not Reachable")||returnData.contains("API KeyGen failed")
-				||returnData.contains("Failed in Broker")||returnData.contains("Failed in adding ID into the ACL")
-				||returnData.contains("LDAP update Failure")|| returnData.contains("Queue Deletion Failure")||returnData.contains("uCat deletion Failure"))
-		{
-			try
-			{
-				String resp=Deregister.removeEntries(getApikey(), entity.getEntityID());
-				System.out.println(resp);
-				
-			}
-			catch(Exception e)
-			{
+
+		returnData = createEntityJSONParser.JSONParser();
+		System.out.println(returnData);
+
+		if (returnData.contains("ID already used")) {
+			resp.setStatus(CONFLICT);
+			return resp;
+		}
+
+		else if (returnData.contains("uCat update Failure") || returnData.contains("Server Not Reachable")
+				|| returnData.contains("API KeyGen failed") || returnData.contains("Failed in Broker")
+				|| returnData.contains("Failed in adding ID into the ACL") || returnData.contains("LDAP update Failure")
+				|| returnData.contains("Queue Deletion Failure") || returnData.contains("uCat deletion Failure")) {
+			try {
+				String response = Deregister.removeEntries(getApikey(), entity.getEntityID());
+				System.out.println(response);
+
+			} catch (Exception e) {
 				System.out.println(e.getMessage());
 			}
-			
-			return Response.status(Response.Status.SERVICE_UNAVAILABLE).entity(returnData).build();
+			resp.setStatus(SERVICE_UNAVAILABLE);
+			return resp;
 		}
-			 
-		
-		else if(returnData.contains("ID not provided")||returnData.contains("Security level must be between 1-5")||returnData.contains("JSON parse error")||returnData.contains("Possible missing fields")||returnData.contains("Missing LoRa information")||returnData.contains("Missing Video information")
-				||returnData.contains("Cannot Onboard Video camera in VideoServer. POST Error.")||returnData.contains("serverConfiguration_credentials, some field not found in json")
-				||returnData.contains("PlayURL is not specified in json"))
-		{
-			try
-			{
-				String resp=Deregister.removeEntries(getApikey(), entity.getEntityID());
-				System.out.println(resp);
-				
-			}
-			catch(Exception e)
-			{
+
+		else if (returnData.contains("ID not provided") || returnData.contains("Security level must be between 1-5")
+				|| returnData.contains("JSON parse error") || returnData.contains("Possible missing fields")
+				|| returnData.contains("Missing LoRa information") || returnData.contains("Missing Video information")
+				|| returnData.contains("Cannot Onboard Video camera in VideoServer. POST Error.")
+				|| returnData.contains("serverConfiguration_credentials, some field not found in json")
+				|| returnData.contains("PlayURL is not specified in json")) {
+			try {
+				String response = Deregister.removeEntries(getApikey(), entity.getEntityID());
+				System.out.println(response);
+
+			} catch (Exception e) {
 				System.out.println(e.getMessage());
 			}
-			return Response.status(Response.Status.BAD_REQUEST).entity(returnData).build();
-		}
-			 
-	
-		else
-		return Response.ok(returnData, MediaType.APPLICATION_JSON).build();
+			resp.setStatus(BAD_REQUEST);
+			return resp;
+		} else
+			resp.setStatus(200, returnData);
+		return resp;
 	}
-	
-	@DELETE
-	@Produces(MediaType.APPLICATION_JSON)
-	public String deleteAPIKey(@Context HttpServletRequest request) {
-		
+
+	public String deleteAPIKey(HttpServletRequest request) {
+
 		try {
 			getHeaderInfo(request);
 			body = getBody(request);
@@ -114,90 +121,84 @@ public class RequestRegister {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	
+
 		String returnData = DeleteEntityJSONParser.JSONParser();
-		System.out.println(returnData);	
-		
+		System.out.println(returnData);
+
 		return returnData;
 	}
-	
+
 	private void getHeaderInfo(HttpServletRequest request) {
 		// TODO Auto-generated method stub
 		System.out.println("In Request Header");
-		
+
 		System.out.println("------------HEADERS----------------");
-		
+
 		authorization = request.getHeader("authorization");
-		System.out.println("Authorisation="+authorization);
-		
+		System.out.println("Authorisation=" + authorization);
+
 		X_Consumer_Custom_ID = request.getHeader("X-Consumer-Custom-ID");
-		System.out.println("Custom ID="+X_Consumer_Custom_ID);
-		
+		System.out.println("Custom ID=" + X_Consumer_Custom_ID);
+
 		X_Consumer_Username = request.getHeader("X-Consumer-Username");
-		System.out.println("Username="+X_Consumer_Username);
-		
+		System.out.println("Username=" + X_Consumer_Username);
+
 		apikey = request.getHeader("apikey");
-		System.out.println("Apikey="+apikey);
-		
-		try
-		{
-		security_level=request.getHeader("security_level");
-		}
-		catch(Exception e)
-		{
+		System.out.println("Apikey=" + apikey);
+
+		try {
+			security_level = request.getHeader("security_level");
+		} catch (Exception e) {
 			System.out.println("Security Level not specififed");
-		}
-		finally
-		{
-			if(security_level==null)
-			{
+		} finally {
+			if (security_level == null) {
 				System.out.println("Using default security level of 4");
-			    security_level=String.valueOf(4);	
+				security_level = String.valueOf(4);
 			}
 		}
-		
+
 		System.out.println(security_level);
 		X_Consumer_Groups = request.getHeader("X-Consumer-Groups");
 		System.out.println(X_Consumer_Groups);
-		
+
 		System.out.println("------------HEADERS----------------");
 	}
 
 	private String getBody(HttpServletRequest request) throws IOException {
 		// TODO Auto-generated method stub
-		
+
 		System.out.println("In Request Body");
-		
+
 		String body = null;
-	    StringBuilder stringBuilder = new StringBuilder();
-	    BufferedReader bufferedReader = null;
+		StringBuilder stringBuilder = new StringBuilder();
+		BufferedReader bufferedReader = null;
 
-	    try {
-	        InputStream inputStream = request.getInputStream();
-	        if (inputStream != null) {
-	            bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-	            char[] charBuffer = new char[128];
-	            int bytesRead = -1;
-	            while ((bytesRead = bufferedReader.read(charBuffer)) > 0) {
-	                stringBuilder.append(charBuffer, 0, bytesRead);
-	            }
-	        } else {
-	            stringBuilder.append("");
-	        }
-	    } catch (IOException ex) {
-	        throw ex;
-	    } finally {
-	        if (bufferedReader != null) {
-	            try {
-	                bufferedReader.close();
-	            } catch (IOException ex) {
-	                throw ex;
-	            }
-	        }
-	    }
+		try {
+			InputStream inputStream = request.getInputStream();
+			if (inputStream != null) {
+				bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+				char[] charBuffer = new char[128];
+				int bytesRead = -1;
+				while ((bytesRead = bufferedReader.read(charBuffer)) > 0) {
+					stringBuilder.append(charBuffer, 0, bytesRead);
+				}
+			} else {
+				stringBuilder.append("");
+			}
+		} catch (IOException ex) {
+			throw ex;
+		} finally {
+			if (bufferedReader != null) {
+				try {
+					bufferedReader.close();
+				} catch (IOException ex) {
+					throw ex;
+				}
+			}
+		}
 
-	    body = stringBuilder.toString();
-	    return body;
+		body = stringBuilder.toString();
+		return body;
 	}
 
 	public static String getAuthorization() {
@@ -219,9 +220,8 @@ public class RequestRegister {
 	public static String getApikey() {
 		return apikey;
 	}
-	
-	public static String getSecurityLevel()
-	{
+
+	public static String getSecurityLevel() {
 		return security_level;
 	}
 
@@ -232,7 +232,4 @@ public class RequestRegister {
 	public static String getBody() {
 		return body;
 	}
-
-	
-
 }
--- src/rbccps/smartcity/IDEAM/APIs/cdxapis.java
+++ src/rbccps/smartcity/IDEAM/APIs/cdxapis.java
@@ -13,6 +13,7 @@ public class cdxapis {
 	static DeploymentManager manager;
 	
 	public static void main(String... args) throws Exception {
+		
 		servletBuilder = Servlets.deployment().setClassLoader(cdxapis.class.getClassLoader());
 		servletBuilder.setDeploymentName("cdx").setContextPath("/cdx");
 		
@@ -20,11 +21,12 @@ public class cdxapis {
 		servletBuilder.addServlets(Servlets.servlet("follow", new RequestFollow().getClass()).addMapping("/follow"));
 		servletBuilder.addServlets(Servlets.servlet("share", new RequestShare().getClass()).addMapping("/share"));
 		servletBuilder.addServlets(Servlets.servlet("search", new RequestSearch().getClass()).addMapping("/search"));
+		servletBuilder.addServlets(Servlets.servlet("register", new RequestRegister().getClass()).addMapping("/register"));
 		
 		manager = Servlets.defaultContainer().addDeployment(servletBuilder);
 		manager.deploy();
 		PathHandler path = Handlers.path(Handlers.redirect("/cdx")).addPrefixPath("/cdx", manager.start());
-		Undertow server = Undertow.builder().addHttpListener(8888, "localhost").setHandler(path).build();
+		Undertow server = Undertow.builder().addHttpListener(8080, "0.0.0.0").setHandler(path).build();
 		server.start();
 	}
 }
